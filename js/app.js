b2925580fea8ee5a7c7f1beecb3d18e7db2ae652
Diogo Freitas Castro <diogofreitascastro@gmail.com>
2020-10-16 17:02:07 +0100

    + hamburger

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
 $(document).ready(function() {
+	var $hamburger = $(".hamburger");
+	$hamburger.on("click", function(e) {
+		$hamburger.toggleClass("is-active");
+    // Do something else, like open/close menu
+  });
 });
