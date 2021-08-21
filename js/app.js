7a22941eff6a73cdc6f4a1a18efdc216e5428ba6
Diogo Freitas Castro <diogofreitascastro@gmail.com>
2021-08-21 22:25:53 +0100

    changed filters hover into click toggle

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+	/* dropdown menu */
+
 	$('.hamburger').click(function(){
 		$(this).toggleClass('open');
 		$('.drop-down-menu').toggleClass('open');
@@ -6,5 +9,12 @@ $(document).ready(function(){
 	$('.drop-down-menu').click(function(){
 		$(this).removeClass('open');
 		$('.hamburger').removeClass('open');
+	});
+
+
+	/* toggle filters */
+
+	$('.filters li').click(function(){
+		$(this).toggleClass('active-filter');
 	});
 });
